afaa4bbcc924ecc76a6a2a3fc7085ffa10f87e8d
felipoxy1 <luizfelipepassos2015@gmail.com>
2025-08-25 13:42:15 -0300

    chore: Remove commented CORS configuration line in Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-// Adiciona a configuração de CORS.
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: MyAllowSpecificOrigins,
